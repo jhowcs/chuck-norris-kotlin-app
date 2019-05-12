b72150828a649113e6dfa184c1f3a7a82d130a84
jonathan <jhowcs@hotmail.com>
2019-05-12 18:47:46 +0100

    Configure DI

--- app/src/main/java/com/jhowcs/chucknorrisapp/di/AppModule.kt
+++ app/src/main/java/com/jhowcs/chucknorrisapp/di/AppModule.kt
@@ -1,10 +1,17 @@
 package com.jhowcs.chucknorrisapp.di
 
+import com.jhowcs.chucknorrisapp.BaseSchedulers
+import com.jhowcs.chucknorrisapp.SchedulersImpl
+import com.jhowcs.chucknorrisapp.presentation.JokeViewModel
 import com.jhowcs.chucknorrisapp.repository.remote.Api
 import com.jhowcs.chucknorrisapp.repository.remote.BaseApi
+import com.jhowcs.chucknorrisapp.repository.remote.JokeRepository
 import org.koin.dsl.module
 
 val appModule = module {
 
     single<BaseApi> { Api() }
+    single<BaseSchedulers> { SchedulersImpl() }
+    factory { JokeRepository(get()) }
+    single { JokeViewModel(get(), get())  }
 }
