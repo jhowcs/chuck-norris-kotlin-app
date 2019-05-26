256c8f94f1be3e6e9797207ac8501d8901b3bad9
jonathan <jhowcs@hotmail.com>
2019-05-26 11:48:29 +0100

    Add more UI tests to validate scenarios:
    - should return a different joke on each click
    - should retain joke on screen rotation

--- app/src/androidTest/java/com/jhowcs/chucknorrisapp/config/TestApplication.kt
+++ app/src/androidTest/java/com/jhowcs/chucknorrisapp/config/TestApplication.kt
@@ -1,12 +1,10 @@
 package com.jhowcs.chucknorrisapp.config
 
 import android.app.Application
-import org.koin.core.context.startKoin
 
 class TestApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        startKoin {}
     }
 }
--- app/src/androidTest/java/com/jhowcs/chucknorrisapp/presentation/MainActivityTest.kt
+++ app/src/androidTest/java/com/jhowcs/chucknorrisapp/presentation/MainActivityTest.kt
@@ -1,33 +1,34 @@
 package com.jhowcs.chucknorrisapp.presentation
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
 import com.jhowcs.chucknorrisapp.BaseSchedulers
 import com.jhowcs.chucknorrisapp.R
 import com.jhowcs.chucknorrisapp.data.JokeApi
 import com.jhowcs.chucknorrisapp.di.appModuleTest
 import com.jhowcs.chucknorrisapp.repository.remote.JokeService
 import io.mockk.every
+import io.mockk.verify
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import org.hamcrest.CoreMatchers.allOf
-import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.core.context.loadKoinModules
-import org.koin.core.context.stopKoin
-import org.koin.test.KoinTest
+import org.koin.core.context.startKoin
+import org.koin.test.AutoCloseKoinTest
 import org.koin.test.inject
 
 @RunWith(AndroidJUnit4::class)
-class MainActivityTest: KoinTest {
+class MainActivityTest : AutoCloseKoinTest() {
 
     private val jokeService: JokeService by inject()
     private val scheduler: BaseSchedulers by inject()
@@ -37,12 +38,10 @@ class MainActivityTest: KoinTest {
 
     @Before
     fun setup() {
-        loadKoinModules(appModuleTest)
-    }
+        startKoin { modules(appModuleTest) }
 
-    @After
-    fun tearDown() {
-        stopKoin()
+        every { scheduler.io() } returns Schedulers.trampoline()
+        every { scheduler.ui() } returns Schedulers.trampoline()
     }
 
     @Test
@@ -54,13 +53,48 @@ class MainActivityTest: KoinTest {
         )
 
         every { jokeService.fetchRandomJoke() } returns Observable.just(joke)
-        every { scheduler.io() } returns Schedulers.trampoline()
-        every { scheduler.ui() } returns Schedulers.trampoline()
 
-        val scenario = ActivityScenario.launch(MainActivity::class.java)
-        scenario.moveToState(Lifecycle.State.RESUMED)
+        ActivityScenario.launch(MainActivity::class.java)
 
         onView(allOf(withId(R.id.random_joke), withText("The Bearded Lady at Ringling Bros.")))
             .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun whenClickOnGetJoke_ShouldFetchANewJoke() {
+        val joke1 = JokeApi("id_1", "http://url_1", "Value 1")
+        val joke2 = JokeApi("id_2", "http://url_2", "Value 2")
+
+        every { jokeService.fetchRandomJoke() } returnsMany
+                listOf(Observable.just(joke1), Observable.just(joke2))
+
+        ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.btnGetJoke)).perform(click())
+
+        onView(allOf(withId(R.id.random_joke), withText("Value 2")))
+            .check(matches(isDisplayed()))
+
+        verify(exactly = 2) { jokeService.fetchRandomJoke() }
+    }
+
+    @Test
+    fun whenStartActivityShouldShowARandomJokeAndWhenRotateScreen_ShouldShowTheSameJoke() {
+        val joke1 = JokeApi("id_1", "http://url_1", "Value 1")
+
+        every { jokeService.fetchRandomJoke() } returns Observable.just(joke1)
+
+        ActivityScenario.launch(MainActivity::class.java)
+
+        onView(allOf(withId(R.id.random_joke), withText("Value 1")))
+            .check(matches(isDisplayed()))
+
+        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        device.setOrientationLeft()
+
+        onView(allOf(withId(R.id.random_joke), withText("Value 1")))
+            .check(matches(isDisplayed()))
+
+        verify(exactly = 1) { jokeService.fetchRandomJoke() }
     }
 }
