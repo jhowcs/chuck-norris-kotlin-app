8a28b30a2d96236a5224c6d2c27fc09203423902
jonathan <jhowcs@hotmail.com>
2019-11-12 20:09:37 +0000

    Remove unused code

--- app/src/androidTest/java/com/jhowcs/chucknorrisapp/di/AppModuleTest.kt
+++ app/src/androidTest/java/com/jhowcs/chucknorrisapp/di/AppModuleTest.kt
@@ -2,8 +2,6 @@ package com.jhowcs.chucknorrisapp.di
 
 import com.jhowcs.chucknorrisapp.BaseSchedulers
 import com.jhowcs.chucknorrisapp.presentation.JokeViewModel
-import com.jhowcs.chucknorrisapp.repository.remote.Api
-import com.jhowcs.chucknorrisapp.repository.remote.BaseApi
 import com.jhowcs.chucknorrisapp.repository.remote.JokeRepository
 import com.jhowcs.chucknorrisapp.repository.remote.JokeService
 import io.mockk.mockk
@@ -12,7 +10,6 @@ import org.koin.dsl.module
 
 val appModuleTest = module {
 
-    single<BaseApi> { Api() }
     single { mockk<BaseSchedulers>() }
 
     single { mockk<JokeService>() }
